29f99dca32974ef45fdaede643d4a0271acf67cd
damiano.fossa <d.fossa.91@gmail.com>
2018-10-09 23:44:16 +0200

    server setup

---
+++ server/model/db.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+var dburl = 'mongodb://localhost:27017/User';
+var retry = null;
+mongoose.connect(dburl);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to ' + dburl);
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+function gracefulShutdown(msg, callback) {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+}
+
+// For nodemon restarts
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('App termination (SIGINT)', function() {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  gracefulShutdown('App termination (SIGTERM)', function() {
+    process.exit(0);
+  });
+});
+
+// SCHEMAS & MODELS
---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+// Routes
+
+// router
+// 	.route('/')
+// 	.get = (req, res) => {
+//     		res.send('Hello');
+//   		}
+
+module.exports = router;
