7292a894313db9b9cbe7550818ee3166435b0822
Damiano Fossa <d.fossa.91@gmail.com>
2018-10-16 22:18:27 +0200

    Router components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Homepage from './Pages/Homepage'
+import Polls from './Pages/Polls'
+import SinglePoll from './Pages/SinglePoll'
+import NoMatch from './Pages/NoMatch'
+import Header from './Header'
+import Footer from './Footer'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChartBar, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+
 import './App.css';
 import 'bulma/css/bulma.css'
-import Homepage from './Homepage/Homepage'
+
+library.add(faChartBar);
+library.add(faExternalLinkAlt);
 
 class App extends Component {
   render() {
     return (
-      <Homepage />
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/polls" component={Polls} />
+            <Route exact path="/polls/:id" component={SinglePoll} />
+            <Route component={NoMatch} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
