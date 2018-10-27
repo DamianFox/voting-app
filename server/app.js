661a153ff8d1356348b0066b85b4441379ababee
Damiano Fossa <d.fossa.91@gmail.com>
2018-10-27 16:44:21 +0200

    Backend side of the Twitter authentication

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
+require('./model/db.js');
 var express = require('express');
 var app = express();
-// var path = require('path');
 var bodyParser = require('body-parser');
 const cors = require('cors');
-var routes = require('./routes');
+var routes = require('./routes/routes.js');
+var passport = require('passport');
+const session = require('express-session');
 
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', 5000);
 
 // Add middleware to console log every request
 app.use((req, res, next) => {
@@ -17,16 +19,25 @@ app.use((req, res, next) => {
 // Set static directory before defining routes
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-app.use(cors());
+// enable cors
+var corsOption = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+app.use(cors(corsOption));
 
 // Enable parsing of posted forms
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routing
-app.use('/api', (req, res) => {
-		res.send('Hello');
-	});
+app.use('/api', routes);
 
 // Listen for requests
 var server = app.listen(app.get('port'), () => {
