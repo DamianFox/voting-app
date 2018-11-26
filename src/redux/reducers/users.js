98af9460905430e02cddf1d407113862da8fae94
Damiano Fossa <d.fossa.91@gmail.com>
2018-11-27 00:20:28 +0100

    functions used to persist the State

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -12,11 +12,7 @@ export default (state=initialState, action) => {
     case 'LOGIN_ERROR':
       return Object.assign({}, state, {token: null, isAuthenticated: false, user: null});
     case 'LOGIN_SUCCESS':
-      console.log('LOGIN_SUCCESS in reducers');
-      console.log('Action in LOGIN_SUCCESS', action);
-      // localStorage.setItem('user', action.payload.user);
       localStorage.setItem('token', action.payload.token);
-      // localStorage.setItem('todos', JSON.stringify(this.state.items));
       return Object.assign({}, state, {token: action.payload.token, isAuthenticated: true, user: action.payload.user });
     default:
       return state;
