5829692726c9749186bc8b7262d51e48d9ff79e1
Damiano Fossa <d.fossa.91@gmail.com>
2018-11-28 15:44:00 +0100

    Page that shows the list of polls

--- src/Pages/Polls.js
+++ src/Pages/Polls.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadPolls } from '../redux/actions/actions';
+
 import 'bulma/css/bulma.css'
+import PollsList from './PollsList';
 
 class Polls extends Component {
+
   render() {
+  	const polls = this.props.polls;
+    
     return (
-      <div>
-        <h1 className="title has-text-centered">Polls</h1>
-      </div>
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div className="column is-6 is-offset-3">
+              <h3 className="title has-text-grey">Polls</h3>
+            </div>
+            <PollsList polls={polls} />
+          </div>
+
+        </div>
+      </section>
     );
   }
 }
 
-export default Polls;
+const mapStateToProps = (state) => {
+  return {
+    token: state.users.token,
+    user: state.users.user,
+    isAuthenticated: state.users.isAuthenticated,
+    polls: state.polls.polls
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+      loadPolls: () => dispatch(loadPolls())
+  };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Polls);
